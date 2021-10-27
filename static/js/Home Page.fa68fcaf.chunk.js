a1a62eb9aaa119066ba78705e9f802d3a0118c6e
Николай Гребенюк <Yanukhous@ukr.net>
2021-10-27 10:40:45 +0300

    Updates

---
+++ static/js/Home Page.fa68fcaf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-35-module-5"]=this["webpackJsonpreact-35-module-5"]||[]).push([[0],{83:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var c=n(1);function r(){return Object(c.jsx)("h1",{children:"Home"})}}}]);
+//# sourceMappingURL=Home Page.fa68fcaf.chunk.js.map
